1cade346bb3e248820a91ef805e579d305d759b7
W3STi <71008971+W3STi@users.noreply.github.com>
2022-09-25 14:15:35 +0300

    Сделали наполнение форм

--- Medic/mainwindow.cpp
+++ Medic/mainwindow.cpp
@@ -13,3 +13,43 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::on_pushButton_clicked()
+{
+    form1.show();
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    form2.show();
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    form3.show();
+}
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    form4.show();
+}
+
+void MainWindow::on_pushButton_5_clicked()
+{
+    form5.show();
+}
+
+void MainWindow::on_pushButton_6_clicked()
+{
+    form6.show();
+}
+
+void MainWindow::on_pushButton_7_clicked()
+{
+    form7.show();
+}
+
+void MainWindow::on_pushButton_8_clicked()
+{
+    form8.show();
+}
+
